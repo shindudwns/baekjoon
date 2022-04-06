21410cdcb9111ae89df670b71972ee5166f7f680
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-04-06 22:13:01 +0900

    Create 1297.cpp

---
+++ algorithm/Math/problem/1297.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath> 
+using namespace std; 
+int main() { 
+int len,height,width; cin >> len >> height >> width; cout << (int)(len/sqrt(height*height+width*width)*height) << " " <<(int)(len/sqrt(height*height+width*width)*width)<<"\n";
+            return 0; }

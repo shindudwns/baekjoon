ce60a81e4d2b760e0f09bcf4148422e02cd0c505
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-04-18 17:05:53 +0900

    Create 2225.cpp

---
+++ algorithm/DP/problem/2225.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<limits.h>
+
+using namespace std;
+
+int n, k;
+int st = 0;
+long long dp[201][201];
+
+void DP() {
+    for (int i = 0; i <= n; i++)
+    {
+        dp[1][i] = 1;
+    }
+
+    for (int l = 2; l <= k; l++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            for (int i = 0; i <= j; i++)
+            {
+                dp[l][j] = dp[l][j] + dp[l - 1][i];
+            }
+            dp[l][j] %= 1000000000;
+        }
+    }
+}
+
+int main(void) {
+
+	cin >> n >> k;
+    DP();
+    cout << dp[k][n] << endl;
+}

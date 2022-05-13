d6a5dd8524eb08eead3e61a27149337ed762b7c2
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-05-13 20:49:16 +0900

    Create 15894.cpp

---
+++ algorithm/Printf/problem/15894.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    unsigned int N;
+    cin>>N;
+    cout<<4*N;
+    return 0;
+}

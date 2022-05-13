69e815e06ac576ce3eb2b57682714b7f06d3c300
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-05-13 20:47:29 +0900

    Create 14645.cpp

---
+++ algorithm/Printf/problem/14645.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std; 
+int main() { int n, k, a, b; scanf("%d%d", &n, &k);
+            for (int i = 0; i < n; i++) scanf("%d%d", &a, &b); 
+            printf("비와이"); return 0; 
+     
+           }

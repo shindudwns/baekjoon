2cf19085bb1a4fa8670d188ff7d4f714d4222375
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-08-02 17:33:23 +0900

    Create 10039.cpp

---
+++ algorithm/Printf/problem/10039.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() 
+{
+    int a = 0;
+    int sum =0;
+    
+    for( int i =0 ;i<5;i++)
+    {
+    	cin >> a;
+        
+        if( a < 40 )
+          a = 40;
+          
+        sum += a; 
+    }
+    
+    cout << sum/5;
+    
+    return 0;
+}

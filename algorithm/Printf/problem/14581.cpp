b7079bd7447dab57f7019533f04873de08993939
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-08-22 19:39:22 +0900

    Create 14581.cpp

---
+++ algorithm/Printf/problem/14581.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	string str;
+	cin >> str;
+
+	string fan = ":fan:";
+
+	cout << fan << fan << fan<<"\n";
+	cout << fan << ":" << str << ":" << fan << "\n";
+	cout << fan << fan << fan;
+
+}

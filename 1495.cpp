7f81265ce768371ad1c3a4ecf71521c3f570bddf
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-09-03 14:03:46 +0900

    Create 1495.cpp

---
+++ 1495.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string fa(string x)
+{
+    string tmp;
+    tmp = ((x[0])-'0' * x.size()) + '0';
+    return tmp;
+}
+int main() {
+    string x;
+    cin >> x;
+    for (int i = 0; i < x.size(); i++)
+    {
+        x = fa(x);
+    }
+    if (x.size() == 1)
+        cout << "FA" << '\n';
+    else
+        cout << "NFA" << '\n';
+}

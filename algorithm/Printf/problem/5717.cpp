86f5c5c1e86693ce3b0de80f4508cfe7d1914fde
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-20 20:50:46 +0900

    Create 5717.cpp

---
+++ algorithm/Printf/problem/5717.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m, f;
+    while (1)
+    {
+        cin >> m >> f;
+        if (m == 0 && f == 0)
+            break;
+        cout << m + f << '\n';
+    }
+}

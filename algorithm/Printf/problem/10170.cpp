ac0ae06ba54d1f8431adfbf351102855c3c7a0d7
shin is god <97870136+shindudwns@users.noreply.github.com>
2023-01-09 00:25:02 +0900

    Update 10170.cpp

--- algorithm/Printf/problem/10170.cpp
+++ algorithm/Printf/problem/10170.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main () {
     cout << "NFC West       W   L  T" << endl
     << "-----------------------" << endl
-    << "Seattle        13  3  0" << endl
+    << "Seattle        13  3  0" << end
     << "San Francisco  12  4  0" << endl
     << "Arizona        10  6  0" << endl
     << "St. Louis      7   9  0" << endl << endl
